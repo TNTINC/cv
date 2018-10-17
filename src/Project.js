bfb26f9fdd80875ef0b531883143d5ab42ffabf1
Friðfinnur M <notandinotandi@gmail.com>
2018-10-17 22:24:03 +0000

    Tweaked Project class to generate new rotation on mouseover, updated Portfolio class to fix missing key.

--- src/Project.js
+++ src/Project.js
@@ -4,22 +4,33 @@ class Project extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      rotation: Math.random() * 4 - 2
-    }
+      rotation: Math.random() * 4.0 - 2.0,
+    };
   }
 
   render() {
     return (
-      <div 
+      <div
         className="Project"
-        style={{transform: `rotate(${this.state.rotation}deg)`}}  >
-        <a href={ this.props.link }>
-        <img src={ this.props.image } alt={ this.props.name } />
-        <h3> { this.props.name } </h3>
-        <p> { this.props.description } </p>
+        style={{ transform: `rotate(${this.state.rotation}deg)` }}
+        onMouseEnter={this.onMouseEnter.bind(this)}
+      >
+        <a href={this.props.link}>
+          <img src={this.props.image} alt={this.props.name} />
+          <h3> {this.props.name} </h3>
+          <p> {this.props.description} </p>
         </a>
       </div>
-    )
+    );
+  }
+
+  onMouseEnter() {
+    // Set a new rotation each time the user hovers over a project.
+    this.setState(state => {
+      return Object.assign({}, state, {
+        rotation: Math.random() * 4.0 - 2.0,
+      });
+    });
   }
 }
 
